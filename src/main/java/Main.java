b7001873c3c4cca686098900ad9dac448f4ac531
Бабкин Алексей <babkin.92@mail.ru>
2023-12-18 22:01:04 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Post post = new Post();
+        
+        post.name = "Иван";
+        post.passport = "4444 № 44444444";
+        post.patronymic = "Иванович";
+        post.phone = "+7 (999)-999-99-99";
+        post.surname = "Иванов";
+        post.subscription = Boolean.parseBoolean(String.valueOf(true));
+        post.birthday = new FormDate();
+        post.birthday.day = Integer.parseInt("13");
+        post.birthday.month = Integer.parseInt("6");
+        post.birthday.year = Integer.parseInt("1999");
+    }
+}
